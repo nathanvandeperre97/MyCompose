c41b16e484e1eeef01c4670369dd183c15d6c894
nathanvandeperre97 <nathanvandeperre@gmail.com>
2021-08-23 17:24:26 +0200

    fix(dependencies) : fix dependecies errors

--- app/src/main/java/com/example/mycompose/MainActivity.kt
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -1,42 +1,54 @@
 package com.example.mycompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import coil.compose.rememberImagePainter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SetText("Hello world!")
+            SetText("This is a bird !")
         }
     }
 }
 
 @ExperimentalAnimationApi
 @Composable
-fun SetText(name: String) {
+fun SetText(text: String) {
     Card {
         var isExpanded by remember { mutableStateOf(false) }
-        Column(Modifier.clickable { isExpanded = !isExpanded }) {
-            Image(
-                painter = rememberImagePainter("https://fr.wikipedia.org/wiki/Twitter#/media/Fichier:Twitter_Bird.svg"),
-                contentDescription = null,
-            )
-            AnimatedVisibility(isExpanded) {
-                Text(text = "This is a bird")
-            }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier.clickable { isExpanded = !isExpanded },) {
+                    val image: Painter = painterResource(id = R.mipmap.ic_twitter_image_foreground)
+                    Image(
+                        painter = image,
+                        contentDescription = "bird image",
+                    )
+                    AnimatedVisibility(isExpanded) {
+                        Text(text = text)
+                    }
+                }
         }
     }
 }
